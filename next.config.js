8b2ea72256c2d8054678c604af2ede32e0865c77
Rodrigo Brito <spensmusic@gmail.com>
2021-07-23 12:44:45 -0300

    fix url deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    localAPI: "http://localhost:3333",
+    localAPI: "https://trabseg-api.herokuapp.com",
     //se localmente="http://localhost:3333"
     //se em prod   ="https://trabseg-api.herokuapp.com"
   },
